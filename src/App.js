e6330311e80945bf0812fa9a56911433fef30bc3
Aimee Zawacki <aimee@tna.digital>
2018-07-06 10:31:55 -0600

    passing down items array to all components and programatically generating list items.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import CartHeader from './header.js';
 import CartFooter from './footer.js';
-import CartItems from './CartItems.js'
+import CartItems from './items.js'
+import CartItem from './item.js'
 
 class App extends Component {
+
   render() {
+    const cartItemsList = [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 }
+    ]
     return (
       <div>
         <CartHeader />
-        <CartItems />
-        <CartFooter />
+        <CartItems items={cartItemsList}/>
+        <CartFooter copyright="2018" />
       </div>
     );
   }
